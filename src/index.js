e096af7a4a5cc690cf6305ea2fbaf8bc6e56eb30
abasalilov <abasalilov@gmail.com>
2018-05-09 04:47:08 -0700

    aupdate tests

--- src/index.js
+++ src/index.js
@@ -1,41 +1,109 @@
-import axios from 'axios';
+import axios from "axios";
+import { CarQuery } from "car-query";
 
-const baseURL = [ 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/','?format=json&modelyear='];
+const baseURL = [
+  "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/",
+  "?format=json&modelyear="
+];
 
-const makeVinReq = (url) => (
-   axios.create({
+export const makeNHTSAReq = url =>
+  axios.create({
     timeout: 10000,
-    method: 'get',
-    url: url.replace(/\s/g, '')
-  })
-)
+    method: "get",
+    url: url.replace(/\s/g, "")
+  });
 
-const sortData = (data) =>{
-  const nonEmptyData = {}
-    for(const key in data){
-      if(data[key] !== ''){
-          nonEmptyData[key] = data[key]
-      }
+const sortData = data => {
+  const nonEmptyData = {};
+  for (const key in data) {
+    if (data[key] !== "") {
+      nonEmptyData[key] = data[key];
     }
-    nonEmptyData['results'] = true;
-    return nonEmptyData;
-}
+  }
+  nonEmptyData["results"] = true;
+  return nonEmptyData;
+};
 
 async function decode(vin) {
-  if(vin === undefined || vin.length < 1 || vin === null){
-    return {results: false};
+  if (vin === undefined || vin.length < 1 || vin === null) {
+    return { results: false };
   }
   const url = baseURL[0] + vin + baseURL[1];
-  const vinReq = makeVinReq(url);
+  const vinReq = makeNHTSAReq(url);
   try {
-       const vinResponse = await vinReq();
-       return sortData(vinResponse.data.Results[0]);
-
-    } catch(e) {
-       console.log('Error in the request', e)
-       return e;
-    }
+    const vinResponse = await vinReq();
+    return sortData(vinResponse.data.Results[0]);
+  } catch (e) {
+    console.log("Error in the request", e);
+    return e;
+  }
 }
 
+export async function getAllMakes() {
+  const makesReq = makeNHTSAReq(
+    "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+  );
+  try {
+    const makeResponse = await makesReq();
+    return sortMakes(makeResponse.data.Results);
+  } catch (e) {
+    console.log("Error in the request", e);
+    return e;
+  }
+}
+
+export async function models(make) {
+  const url =
+    "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/" +
+    make +
+    "?format=json";
+  const modelsReq = makeNHTSAReq(url);
+  try {
+    const modelsResponse = await modelsReq();
+  } catch (e) {
+    console.log("Error in the request", e);
+    return e;
+  }
+}
 
 export default decode;
+
+export class DropDownMenuFetch {
+  constructor() {
+    this.year = null;
+    this.make = null;
+    this.model = null;
+  }
+
+  updateYear(yr) {
+    this.year = yr;
+  }
+
+  updateMake(make) {
+    this.make = make;
+  }
+}
+
+export async function getNewModels(make) {
+  const makesReq = makeNHTSAReq(
+    "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+  );
+  try {
+    const makeResponse = await makesReq();
+    return sortMakes(makeResponse.data.Results);
+  } catch (e) {
+    console.log("Error in the request", e);
+    return e;
+  }
+}
+
+export async function getYears() {
+  const carQuery = new CarQuery();
+  return carQuery.getYears().then(years => years);
+}
+
+export async function getMakesByYear(yr) {
+  const carQuery = new CarQuery();
+  const makeResults = await carQuery.getMakes(yr).then(makes => makes);
+  return makeResults;
+}
--- test.js
+++ test.js
@@ -1,31 +1,56 @@
-import 'babel-polyfill';
-import 'mocha';
-import {expect} from 'chai'
-import decoder from './src/index.js';
+import "babel-polyfill";
+import axios from "axios";
+import "mocha";
+import { expect } from "chai";
+import decoder, { makes, getYears, getMakesByYear } from "./src/index.js";
 
-const exampleAnswer = {
+const baseURL =
+  "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json";
 
-}
+// export const allMakesRequest = () =>
+//   axios.create({
+//     timeout: 10000,
+//     method: "get",
+//     url: baseURL
+//   });
 
-describe('decoder', () => {
-  it('should return false when vin is empty', async () => {
-    const vinResponse = await decoder()
-    expect(vinResponse.results).to.be.equal(false)
-    });
+const exampleAnswer = {};
 
-    it('should return true when vin entered for a 2002 Acura TL', async () => {
-    const vinResponse = await decoder('19UUA56602A801534')
-    expect(vinResponse.results).to.be.equal(true)
-    });
+describe("decoder", () => {
+  it("should return false when vin is empty", async () => {
+    const vinResponse = await decoder();
+    const make = await makes();
+    expect(vinResponse.results).to.be.equal(false);
+  });
 
-    it('should return data with a ModeYear property of `2002` when vin entered for a 2002 Acura TL', async () => {
-    const vinResponse = await decoder('19UUA56602A801534')
+  it("should return true when vin entered for a 2002 Acura TL", async () => {
+    const vinResponse = await decoder("19UUA56602A801534");
+    expect(vinResponse.results).to.be.equal(true);
+  });
+
+  it("should return data with a ModeYear property of `2002` when vin entered for a 2002 Acura TL", async () => {
+    const vinResponse = await decoder("19UUA56602A801534");
     // console.log('vinResponse', vinResponse)
-    expect(vinResponse.ModelYear).to.be.equal('2002')
-    });
+    expect(vinResponse.ModelYear).to.be.equal("2002");
+  });
 });
 
+const currentYear = new Date().getFullYear();
 
+describe("YMM", () => {
+  it("getYears returns min of 1941 and max of current year", async () => {
+    const years = await getYears();
+    const max = years.maxYear;
+    const min = years.minYear;
+    expect(max).to.be.equal(currentYear);
+    expect(min).to.be.equal(1941);
+  });
+
+  it("getMakesByYear returns an Array of data with ", async () => {
+    const makes = await getMakesByYear(2010);
+    expect(makes.length).to.be.equal(72);
+  });
+});
 
 //SAMPLES TO TEST
 
